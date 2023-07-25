9c58df21b2dc8cf0d976016cf31f1e39f9d2d35a
jalalsadeghi <jalal.a.sadeghi@gmail.com>
2023-07-25 15:45:28 +0330

    change main structure

--- config/urls.py
+++ config/urls.py
@@ -15,3 +15,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(('dp.api.urls', 'api'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# from config.settings.debug_toolbar.setup import DebugToolbarSetup  # noqa
+#
+# urlpatterns = DebugToolbarSetup.do_urls(urlpatterns)
