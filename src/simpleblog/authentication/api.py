e5ac100e440439938b8da1b38541de96934ee014
jalalsadeghi <jalal.a.sadeghi@gmail.com>
2023-11-28 11:41:58 +0330

    Clean Auth file

--- src/simpleblog/authentication/api.py
+++ src/simpleblog/authentication/api.py
@@ -1,9 +1,3 @@
-from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.response import Response
-from rest_framework import status
-from simpleblog.api.mixins import ApiAuthMixin
-
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
